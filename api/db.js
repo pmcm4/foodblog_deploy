2e92fc9ec33ca1def946dea0fe5b45614e505bb1
pmcm <pmorato187@gmail.com>
2023-05-17 13:27:34 +0800

    fix

--- api/db.js
+++ api/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"pmcm4",
-    database:"foodblog"
+    host: "us-cdbr-east-06.cleardb.net",
+    user: "b98ef878cdd7e7",
+    password: "94f44069",
+    database: "heroku_a1b857cdb26bf18"
 })
